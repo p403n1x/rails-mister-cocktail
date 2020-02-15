54a99e7eeafb4d748ab82d09bb5551c4a44f1a9f
olivier bouzon <oliv@hotmail.fr>
2020-02-15 01:10:30 +0100

    Models cocktail & Ingredient created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_141649) do
+ActiveRecord::Schema.define(version: 2020_02_14_232849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2020_02_14_141649) do
   end
 
   create_table "doses", force: :cascade do |t|
-    t.string "description"
+    t.text "description"
     t.bigint "cocktail_id"
     t.bigint "ingredient_id"
     t.datetime "created_at", null: false
@@ -37,16 +37,6 @@ ActiveRecord::Schema.define(version: 2020_02_14_141649) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.text "content"
-    t.integer "rating"
-    t.bigint "cocktail_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["cocktail_id"], name: "index_reviews_on_cocktail_id"
-  end
-
   add_foreign_key "doses", "cocktails"
   add_foreign_key "doses", "ingredients"
-  add_foreign_key "reviews", "cocktails"
 end
