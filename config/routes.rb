54f5fd9fa30baa477d58b754b162baf898459b73
olivier bouzon <oliv@hotmail.fr>
2020-02-17 11:42:24 +0100

    ...

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'cocktails/home'
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#home'
+  resources :cocktails, only: [:home, :index, :show, :new, :create] do
+    resources :ingredients, only: [:new, :show]
+  end
+
 end
